589a02a1da4b5bceea78cdf724608b95ebd84c42
Chris Bonser <bonser.chris@gmail.com>
2021-10-08 10:50:51 -0500

    Add instrumentation

--- addon/services/service-worker-update-notify.js
+++ addon/services/service-worker-update-notify.js
@@ -1,58 +1,61 @@
 import Ember from 'ember'
 import Service from '@ember/service'
-import Evented from '@ember/object/evented';
-import { getOwner } from '@ember/application';
-import { computed } from '@ember/object';
+import Evented from '@ember/object/evented'
+import { getOwner } from '@ember/application'
+import { computed } from '@ember/object'
 import { task, timeout } from 'ember-concurrency'
 import serviceWorkerHasUpdate from '../utils/service-worker-has-update'
 
-const configKey = 'ember-service-worker-update-notify';
-const supportsServiceWorker = typeof navigator !== 'undefined' && 'serviceWorker' in navigator;
+const configKey = 'ember-service-worker-update-notify'
+const supportsServiceWorker =
+  typeof navigator !== 'undefined' && 'serviceWorker' in navigator
 
 async function update() {
+  console.log('>>> register with service worker')
   const reg = await navigator.serviceWorker.register(
     '{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}',
     { scope: '{{ROOT_URL}}' },
-  );
+  )
 
-  return reg.update();
+  return reg.update()
 }
 
 export default Service.extend(Evented, {
-
   hasUpdate: false,
 
-  pollingInterval: computed(function() {
-    let config = getOwner(this).resolveRegistration('config:environment')[configKey];
-    return config && config.pollingInterval || 120000;
+  pollingInterval: computed(function () {
+    let config = getOwner(this).resolveRegistration('config:environment')[
+      configKey
+    ]
+    return (config && config.pollingInterval) || 120000
   }),
 
   pollingTask: task(function* () {
     while (true) {
-      yield update();
+      yield update()
 
-      yield timeout(this.pollingInterval);
+      yield timeout(this.pollingInterval)
     }
   }),
 
   _attachUpdateHandler() {
-    serviceWorkerHasUpdate().then(hasUpdate => {
-      this.pollingTask.cancelAll();
+    serviceWorkerHasUpdate().then((hasUpdate) => {
+      this.pollingTask.cancelAll()
 
       if (hasUpdate) {
-        this.set('hasUpdate', true);
-        this.trigger('update');
+        this.set('hasUpdate', true)
+        this.trigger('update')
       }
     })
   },
 
   init() {
-    this._super(...arguments);
+    this._super(...arguments)
     if (typeof FastBoot === 'undefined') {
-      this._attachUpdateHandler();
+      this._attachUpdateHandler()
       if (!Ember.testing && supportsServiceWorker) {
-        this.pollingTask.perform();
+        this.pollingTask.perform()
       }
     }
-  }
-});
+  },
+})
--- service-worker-registration/index.js
+++ service-worker-registration/index.js
@@ -1,30 +1,34 @@
 import { addSuccessHandler } from 'ember-service-worker/service-worker-registration'
 
 function hasServiceWorkerUpdate(resolve) {
-  addSuccessHandler(function(reg) {
-    reg.onupdatefound = function() {
-      const { installing } = reg;
+  addSuccessHandler(function (reg) {
+    reg.onupdatefound = function () {
+      console.log('>>> onupdatefound')
+      const { installing } = reg
 
-      installing.onstatechange = function() {
+      installing.onstatechange = function () {
+        console.log('>>> onstatechange')
         if (installing.state === 'activated') {
+          console.log(
+            '>>> navigator.serviceWorker.controller',
+            navigator.serviceWorker.controller,
+          )
           resolve(navigator.serviceWorker.controller !== null)
         }
       }
     }
-  });
+  })
 }
 
 // https://caniuse.com/#search=Promise
 // IE11 (2.5%) and Opera Mini (2.3%) do not support Promises
-const arePromisesSupported = 'Promise' in window;
-const disablePolyfill = ({
-  then: function() {
-    return false;
-  }
-});
+const arePromisesSupported = 'Promise' in window
+const disablePolyfill = {
+  then: function () {
+    return false
+  },
+}
 
-window.hasServiceWorkerUpdate =
-  arePromisesSupported
+window.hasServiceWorkerUpdate = arePromisesSupported
   ? new Promise(hasServiceWorkerUpdate)
   : disablePolyfill
-
